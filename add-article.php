7434de348cb52b2cc6f1a98d5bdc16e044b8589e
Taikhun <taiyang78190@gmail.com>
2022-02-16 17:06:16 +0100

    add securization and validation on form for errors messages + css form --DOING

--- add-article.php
+++ add-article.php
@@ -1,3 +1,72 @@
+<?php 
+    const ERROR_REQUIRED = 'Veuillez renseigner ce champ';
+    const ERROR_TITLE_TOO_SHORT = 'Le titre est trop court';
+    const ERROR_CONTENT_TOO_SHORT = 'L\'article est trop court';
+    const ERROR_IMAGE_URL = 'L\'image doit être une url valide';
+
+    $fileName = __DIR__ . '/data/articles.json';
+    $errors = [
+        'title' => '',
+        'image' => '',
+        'category' => '',
+        'content' => ''
+    ];
+    $articles = [];
+
+    // As FILTER SANITIZE STRING is now deprecated I have replaced them with FITLER UNSAFE RAW but we could use html special char to protect against XSS
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (file_exists($fileName)) {
+            $articles = json_decode(file_get_contents($fileName), true) ?? [];
+        }
+        $_POST = filter_input_array(INPUT_POST, [
+            'title' => FILTER_UNSAFE_RAW, 
+            'image' => FILTER_SANITIZE_URL, 
+            'category' => FILTER_UNSAFE_RAW, 
+            'content' => [
+                'filter' => FILTER_UNSAFE_RAW,
+                'flags' => FILTER_FLAG_NO_ENCODE_QUOTES
+                ]
+            ]);
+
+            $title = $_POST['title'] ?? '';
+            $image = $_POST['image'] ?? '';
+            $category = $_POST['category'] ?? '';
+            $content = $_POST['content'] ?? '';
+
+            if (!$title) {
+                $errors['title'] = ERROR_REQUIRED;
+            } elseif (mb_strlen($title) < 5) {
+                $errors['title'] = ERROR_TITLE_TOO_SHORT;               
+            }
+
+            if (!$image) {
+                $errors['image'] = ERROR_REQUIRED;
+            } elseif (!filter_var($image, FILTER_VALIDATE_URL)) {
+                $errors['image'] = ERROR_IMAGE_URL;               
+            }
+            
+            if (!$content) {
+                $errors['content'] = ERROR_REQUIRED;
+            } elseif (mb_strlen($content) < 50) {
+                $errors['content'] = ERROR_CONTENT_TOO_SHORT;               
+            }
+            
+
+            if (empty(array_filter($errors, fn($e) => $e !== ''))) {
+                $articles = [...$articles, [
+                    'title' => $title,
+                    'image' => $image, 
+                    'category' => $category,
+                    'content' => $content,
+                    'id' => time() 
+                ]];
+                file_put_contents($fileName, json_encode($articles));
+                header('Location: /');
+            }             
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -21,13 +90,17 @@
                 <form action="/add-article.php" , method="POST">
                     <div class="form-control">
                         <label for="title">Titre</label>
-                        <input type="text" name="title" id="title">
-                        <!-- <p class="text-error"></p> -->
+                        <input type="text" name="title" id="title"  value=<?= $title ?? '' ?>>
+                        <?php if($errors['title']) : ?>
+                            <p class="text-danger"><?= $errors['title'] ?></p>
+                        <?php endif; ?>
                     </div>
                     <div class="form-control">
                         <label for="image">Image</label>
-                        <input type="text" name="image" id="image">
-                        <!-- <p class="text-error"></p> -->
+                        <input type="text" name="image" id="image" value=<?= $image ?? '' ?>>
+                        <?php if($errors['image']) : ?>
+                            <p class="text-danger"><?= $errors['image'] ?></p>
+                        <?php endif; ?>
                     </div>
                     <div class="form-control">
                         <label for="category">Catégories</label>
@@ -37,16 +110,20 @@
                             <option value="desserts">Desserts</option>
                             <option value="sidedishes">Entrées</option>
                         </select>
-                        <!-- <p class="text-error"></p> -->
+                        <?php if($errors['category']) : ?>
+                            <p class="text-danger"><?= $errors['category'] ?></p>
+                        <?php endif; ?>
                     </div>
                     <div class="form-control">
                         <label for="content">Contenu</label>
-                        <textarea name="content" id="content"></textarea>
-                        <!-- <p class="text-error"></p> -->
+                        <textarea name="content" id="content" value=<?= $content ?? '' ?>></textarea>
+                        <?php if($errors['content']) : ?>
+                            <p class="text-danger"><?= $errors['content'] ?></p>
+                        <?php endif; ?>
                     </div>
                     <div class="form-actions">
                         <a href="/" class="btn btn-secondary" type="button">Annuler</a>
-                        <button class="btn btn-primary" type="button">Sauvegarder</button>
+                        <button type="submit" class="btn btn-primary">Sauvegarder</button>
                     </div>
                 </form>
             </div>
