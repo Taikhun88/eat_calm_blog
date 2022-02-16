59430f2bcfa7ea42b1788ad276ba448c4793715c
Taikhun <taiyang78190@gmail.com>
2022-02-16 14:00:41 +0100

    add style css to add-article.php + creation button link return to index --DONE

--- add-article.php
+++ add-article.php
@@ -7,11 +7,49 @@
     <title>Créer un article - Eat~CALM blog</title>
 </head>
 
+<!-- title -->
+<!-- image -->
+<!-- category -->
+<!-- content -->
+
 <body>
     <div class="container">
         <?php require_once 'includes/header.php' ?>
         <div class="content">
-
+            <div class="block p-20 form-container">
+                <h1>Écrire un article</h1>
+                <form action="/add-article.php" , method="POST">
+                    <div class="form-control">
+                        <label for="title">Titre</label>
+                        <input type="text" name="title" id="title">
+                        <!-- <p class="text-error"></p> -->
+                    </div>
+                    <div class="form-control">
+                        <label for="image">Image</label>
+                        <input type="text" name="image" id="image">
+                        <!-- <p class="text-error"></p> -->
+                    </div>
+                    <div class="form-control">
+                        <label for="category">Catégories</label>
+                        <select name="category" id="category">
+                            <option value="menus">Menus</option>
+                            <option value="meals">Plats</option>
+                            <option value="desserts">Desserts</option>
+                            <option value="sidedishes">Entrées</option>
+                        </select>
+                        <!-- <p class="text-error"></p> -->
+                    </div>
+                    <div class="form-control">
+                        <label for="content">Contenu</label>
+                        <textarea name="content" id="content"></textarea>
+                        <!-- <p class="text-error"></p> -->
+                    </div>
+                    <div class="form-actions">
+                        <a href="/" class="btn btn-secondary" type="button">Annuler</a>
+                        <button class="btn btn-primary" type="button">Sauvegarder</button>
+                    </div>
+                </form>
+            </div>
         </div>
         <?php require_once 'includes/footer.php' ?>
     </div>
