fb599f467e1b23ba600ef5feb568269e294556f6
Taikhun <taiyang78190@gmail.com>
2022-02-17 16:57:17 +0100

    DEBUG categories --DONE

--- index.php
+++ index.php
@@ -6,25 +6,24 @@ $categories = [];
 
 if (file_exists($fileName)) {
     $articles = json_decode(file_get_contents($fileName), true) ?? [];
-    $cattMp = array_map(fn($a) => $a['category'], $articles);
+    $cattMp = array_map(fn ($a) => $a['category'], $articles);
     $categories = array_reduce($cattMp, function ($acc, $cat) {
         if (isset($acc[$cat])) {
             $acc[$cat]++;
-        }else {
+        } else {
             $acc[$cat] = 1;
         }
         return $acc;
     }, []);
 
-    $articlesPerCatagory = array_reduce($articles, function ($acc, $articles) {
-        if(isset($acc[$articles['category']])) {
-            $acc[$articles['category']] = [...$acc[$articles['category']], $articles];
+    $articlesPerCategories = array_reduce($articles, function ($acc, $article) {
+        if (isset($acc[$article['category']])) {
+            $acc[$article['category']] = [...$acc[$article['category']], $article];
         } else {
-            $acc[$articles['category']] = $articles;
+            $acc[$article['category']] = [$article];
         }
         return $acc;
     }, []);
-
 }
 ?>
 
@@ -41,17 +40,23 @@ if (file_exists($fileName)) {
     <div class="container">
         <?php require_once 'includes/header.php' ?>
         <div class="content">
-            <div class="articles-container">
+            <div class="category-container">
+                <?php foreach ($categories as $cat => $num) : ?>
+                    <h2><?= $cat ?></h2>
+                    <div class="articles-container">
 
-                <?php foreach ($articles as $a) : ?>
-                    <div class="article block">
-                        <div class="overflow">
-                            <div class="img-container" style="background-image:url(<?= $a['image'] ?>)">
+                        <?php foreach ($articlesPerCategories[$cat] as $a) : ?>
+                            <div class="article block">
+                                <div class="overflow">
+                                    <div class="img-container" style="background-image:url(<?= $a['image'] ?>)">
+                                    </div>
+                                </div>
+                                <h3><?= $a['title'] ?></h3>
                             </div>
-                        </div>
-                        <h2><?= $a['title'] ?></h2>
+                        <?php endforeach; ?>
                     </div>
                 <?php endforeach; ?>
+
             </div>
 
         </div>
