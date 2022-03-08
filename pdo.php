e5aa424d4d6965eec6a5101a48977a5956ae6058
Taikhun <taiyang78190@gmail.com>
2022-03-08 18:11:28 +0100

    optimization and test of a few fetch methods --DONE

--- pdo.php
+++ pdo.php
@@ -13,6 +13,7 @@
             [
                 // CONSTANT of PDO which enables error messages working with resctrictions for example errors won't display sensitive info
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
             ]
         );
         echo "Connexion à la base de données réussie!";
@@ -21,51 +22,23 @@
         echo "error : " . $e->getMessage();
     }
 
-    // sample of data put in an array so we can send them all with one variable instead of typing one by one
-    // simulates data received via form
-    $user = [
-        'firstname' => "Testtout", 
-        'email' => "testtout@gmail.com", 
-        'password' => 'testtout'
-    ];
+    class User {
+        function greeting() {
+            echo ' <br> Hello ' . $this->firstname . ' ' . $this->email;
+        }
+    }
+    // SELECT request test
+    $statement = $pdo->prepare('SELECT * FROM user WHERE iduser = :id');
+    $statement->bindValue(':id', 15);
+    $users = $statement->execute();
 
-    // we use the question mark here as an indicator / placeholder of an expectue data to be filled
-    // we can replace the question marks by :nameofplaceholder so it's easier to guess the expected data
-    // EX : $stmt->bindParam(':firstname', $user['firstname'], PDO::PARAM_STR);
+    // the attempt of print fails as the boolean result displays 1 as the execute is unable to get and display the data in $users with execute ONLY
+    // to print successfully we need to use fetch Methods fetch or fetchAll
+    // this methods allows to display proper error messages if anything goes wrong
+    // $users = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-    // if it is needed to receive, store a specific type of data within the databases, it can be specified
-    // We call this transtyping, a way to convert by force the sent data
-    // EX : $stmt->bindParam(3, $user['password'], PDO::PARAM_INT);
+    // if we use a class we will have an object so we can execute fetchObject method
+    $users = $statement->fetchObject('User');
+    $users->greeting();
 
-    $statement = $pdo->prepare('INSERT INTO `user` (firstname, email, password) VALUES (
-        ?,
-        ?,
-        ?)        
-    ');
-
-    // A bindvalue step can be included right before the execute, here below an example
-    // in which case we have to remove the data to be execute in the parameters of execute    
-    // bindParam could replace the bindValue method below. 
-    // It allows to load the data at the execution step if it ever changed it's pretty convenient
-
-    // if named placeholders are used then replace the 1st parameter of bind by the namedplaceholders
-    // $statement->bindValue(1, $user['firstname']);
-    // $statement->bindValue(2, $user['email']);
-    // $statement->bindValue(3, $user['password']);
-
-    // as we will receive all data in an array variable we can execute them by selecting with array methods. the question marks are at this execute steps replaced by the variable data.
-    $statement->execute(
-        [
-            $user['firstname'], 
-            $user['email'], 
-            $user['password']
-        ]
-    );
-    // query method version of connexion via PDO. It's a shorter version as it prepares and automatically execute afterwards
-    // for more flexibility and safety it's better to use prepare and execute separately
-    // Also, prepare can be executed multiple times and prevent of SQL injections
-
-    //$statement = $pdo->query('INSERT INTO `user` (firstname, email) VALUES ("Taiyounette","taiyounette@gmail.com")');
-
-    ?>
-</pre>
+    // to avoid typing this FETCH ASSOC method in the fetch method paramater we can include it at TRY step above
