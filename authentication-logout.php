33b64808243d97147168577a6c38ce7f5f85c041
Taikhun <taiyang78190@gmail.com>
2022-03-18 22:08:49 +0100

    creation of authentication files and css files prior integration of code for each page --DONE

---
+++ authentication-logout.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php require_once 'includes/head.php' ?>
+    <title>Se déconnecter - Eat~CALM blog</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php require_once 'includes/header.php' ?>
+        <div class="content">
+            <h1>Se déconnecter</h1>
+        </div>
+        <?php require_once 'includes/footer.php' ?>
+    </div>
+</body>
+
+</html>
--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,20 @@
 <header>
     <a href="/" class="logo">Eat~CALM blog</a>
     <ul class="header-menu">
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/authentication-register.php' ? 'active' : '' ?>>
+            <a href="/authentication-register.php">S'inscrire</a>
+        </li>
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/authentication-login.php' ? 'active' : '' ?>>
+            <a href="/authentication-login.php">Se connecter</a>
+        </li>
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/profile.php' ? 'active' : '' ?>>
+            <a href="/profile.php">Mon profil</a>
+        </li>
         <li class=<?= $_SERVER['REQUEST_URI'] === '/form-article.php' ? 'active' : '' ?>>
             <a href="/form-article.php">Écrire un article</a>
+        </li>
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/authentication-logout.php' ? 'active' : '' ?>>
+            <a href="/authentication-logout.php">Se déconnecter</a>
         </li>
     </ul>
 </header>
