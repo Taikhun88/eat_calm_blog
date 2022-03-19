411c80db8ac11322f84bbd1afbb732c9c46b3d59
Taikhun <taiyang78190@gmail.com>
2022-03-19 23:39:29 +0100

    add login page with filters on input and Sql request to get user data and activate session userid based on --DONE

--- authentication-login.php
+++ authentication-login.php
@@ -1,5 +1,61 @@
 <?php
+$pdo = require_once './database/database.php';
 
+const ERROR_REQUIRED = 'Veuillez renseigner ce champ';
+const ERROR_PASSWORD_TOO_SHORT = 'Le mot de passe choisi doit être composé de minimum 6 caractères';
+const ERROR_EMAIL_INVALID = 'L\'email saisi n\'est pas valide';
+const ERROR_EMAIL_UNKNOWN = 'Cet email n\'est pas reconnue';
+const ERROR_MISMATCH_PASSWORD = 'Veuillez vérifier la saisie du mot de passe';
+
+$errors = [
+    'email' => '',
+    'password' => '',
+];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $input = filter_input_array(INPUT_POST, [
+        'email' => FILTER_SANITIZE_EMAIL,
+    ]);
+    $email = $input['email'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    if (!$email) {
+        $errors['email'] = ERROR_REQUIRED;
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = ERROR_EMAIL_INVALID;
+    }
+
+    if (!$password) {
+        $errors['password'] = ERROR_REQUIRED;
+    } elseif (mb_strlen($password) < 6) {
+        $errors['password'] = ERROR_PASSWORD_TOO_SHORT;
+    }
+
+    if (empty(array_filter($errors, fn ($e) => $e !== ''))) {
+        $statementUser = $pdo->prepare('SELECT * FROM user WHERE email=:email');
+        $statementUser->bindValue(':email', $email);
+        $statementUser->execute();
+        $user = $statementUser->fetch();
+
+        if (!$user) {
+            $errors['email'] = ERROR_EMAIL_UNKNOWN;
+        } else {
+            if (!password_verify($password, $user['password'])) {
+                $errors['password'] = ERROR_MISMATCH_PASSWORD;
+            }   else {
+                $statementSession = $pdo->prepare('INSERT INTO session VALUES (
+                   DEFAULT, 
+                   :userid
+                )');
+                $statementSession->bindValue(':userid', $user['id']);
+                $statementSession->execute();
+                $sessionId = $pdo->lastInsertId();
+                setcookie('session', $sessionId, time() + 60 * 60 * 24 * 14, '', '', false, true);
+                header('Location: /');
+            }
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -7,7 +63,7 @@
 
 <head>
     <?php require_once 'includes/head.php' ?>
-    <link rel="stylesheet" href="/public/css/authentication-login.css">
+    <link rel="stylesheet" href="/public/css/authentication-register.css">
     <title>Se connecter - Eat~CALM blog</title>
 </head>
 
@@ -15,6 +71,30 @@
     <div class="container">
         <?php require_once 'includes/header.php' ?>
         <div class="content">
+            <div class="block p-20 form-container">
+                <h1> Se connecter </h1>
+                <form action="/authentication-login.php" , method="POST">
+
+                    <div class="form-control">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" value="<?= $email ?? '' ?>">
+                        <?php if ($errors['email']) : ?>
+                            <p class="text-danger"><?= $errors['email'] ?></p>
+                        <?php endif; ?>
+                    </div>
+                    <div class="form-control">
+                        <label for="password">Mot de passe</label>
+                        <input type="password" name="password" id="password">
+                        <?php if ($errors['password']) : ?>
+                            <p class="text-danger"><?= $errors['password'] ?></p>
+                        <?php endif; ?>
+                    </div>
+                    <div class="form-actions">
+                        <a href="/" class="btn btn-secondary" type="button">Annuler</a>
+                        <button type="submit" class="btn btn-primary">Connexion</button>
+                    </div>
+                </form>
+            </div>
         </div>
         <?php require_once 'includes/footer.php' ?>
     </div>
