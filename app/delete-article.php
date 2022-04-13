b3263ed9e30c10c8bc44e58b36852bafcb38b1db
Taikhun <taiyang78190@gmail.com>
2022-04-14 01:54:02 +0200

    filter sanitirize special chars on index

--- app/delete-article.php
+++ app/delete-article.php
@@ -5,7 +5,7 @@ $authenticationDB = require_once __DIR__ . '/database/security.php';
 if (!$currentUser) {
     header('Location: /');
 }
-$articleDatabase = require_once __DIR__ . './database/models/ArticleDatabase.php';
+$articleDatabase = require_once __DIR__ . '/database/models/ArticleDatabase.php';
 
 $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_SPECIAL_CHARS);
 
