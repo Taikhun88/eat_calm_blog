710a36617c5568ad15b967b7d6dfc480c4191424
Taikhun <taiyang78190@gmail.com>
2022-04-14 01:00:18 +0200

    remove bug index.php

--- app/show-article.php
+++ app/show-article.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/database/database.php';
 $authenticationDB = require_once __DIR__ . '/database/security.php';
 
 $currentUser = $authenticationDB->isLoggedIn();
-$articleDatabase = require_once __DIR__ . './database/models/ArticleDatabase.php';
+$articleDatabase = require_once __DIR__ . '/database/models/ArticleDatabase.php';
 
 $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $id = $_GET['id'] ?? '';
